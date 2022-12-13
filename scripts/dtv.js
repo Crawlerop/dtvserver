04c856db144c5ae5a7185217d6cc77fb6fbe596e
Your Name <you@example.com>
2022-12-13 19:16:38 +0700

    4MB demux size

--- scripts/dtv.js
+++ scripts/dtv.js
@@ -77,7 +77,7 @@ ExecSignal.once("exec", (args, folders) => {
 RunSignal.once("run", async (params) => {    
    try {
     passed_params = params    
-    var tsp_args = `-I dvb --signal-timeout 10 --guard-interval auto --receive-timeout 10000 --adapter ${params.tuner} --delivery-system DVB-T2 --frequency ${params.frequency}000000 --transmission-mode auto --spectral-inversion off`.split(" ")
+    var tsp_args = `-I dvb --demux-buffer-size 4194304 --signal-timeout 10 --guard-interval auto --receive-timeout 10000 --adapter ${params.tuner} --delivery-system DVB-T2 --frequency ${params.frequency}000000 --transmission-mode auto --spectral-inversion off`.split(" ")
     var folders = []
     var ad_param;
 
