5b6190d5256d1336cbcdc89a892e3a9fea9b5f4a
Your Name <you@example.com>
2022-12-14 12:43:42 +0700

    track stalled status for transcodes

--- scripts/cmds/repeat.js
+++ scripts/cmds/repeat.js
@@ -5,12 +5,27 @@ process.stdin.on("close", () => {
     process.exit(0)
 })
 
+const TIMEOUT_DUR = 30000
+var TIMEOUT_VAL = -1
+
+var LAST_FRAME = -1
+var app;
+
+setInterval(() => {
+    if (TIMEOUT_VAL !== -1 && (Date.now() > TIMEOUT_VAL)) {
+        process.stderr.write(`Transcode stream was stalled for ${args[2]}\n`)
+        app.kill("SIGKILL")
+    }
+}, 2000)
+
 const startProcess = () => {
     //process.stderr.write(args.slice(3).join(" ")+"\n")
-    const app = cp.spawn(args[3], args.slice(4))
+    app = cp.spawn(args[3], args.slice(4))
 
     app.on("exit", () => {
         process.stderr.write(`Restart transcode stream for channel ${args[2]}\n`)
+        LAST_FRAME = -1
+        TIMEOUT_VAL = -1
         startProcess()
     })
 
@@ -18,7 +33,25 @@ const startProcess = () => {
 
     process.stdin.pipe(app.stdin)
     app.stderr.pipe(process.stderr)
-    app.stdout.pipe(process.stdout)
+
+    app.stdout.on("data", (d) => {
+        const chunks = d.toString().replace(/\r/g, "").split("\n")
+        for (let i = 0; i<chunks.length; i++) {
+            if (chunks[i].length >= 0) {
+                const key = chunks[i].split("=")[0]
+                const val = chunks[i].split("=")[1]
+
+                if (key === "frame") {
+                    if (parseInt(val) !== LAST_FRAME) {
+                        LAST_FRAME = parseInt(val)
+                        TIMEOUT_VAL = Date.now() + TIMEOUT_DUR
+                        //process.stderr.write(`Track stalled status\n`)
+                    }
+                }
+            }
+        }
+    })
+    //app.stdout.pipe(process.stdout)
 }
 
 startProcess()
--- scripts/dtv.js
+++ scripts/dtv.js
@@ -111,7 +111,7 @@ RunSignal.once("run", async (params) => {
         const tsp_fork_prm = ["-re", "-y", "-loglevel", current_rendition[0].hwaccel === "nvenc" ? "error": "quiet"].concat(await ffmp_args.genSingle("-", current_rendition, streams, out_folder, params.hls_settings, channel.video.id, channel.audio ? channel.audio.id : 1, audio_filters, true))
         tsp_args.push("-P")
         tsp_args.push("fork")        
-        tsp_args.push(`tsresync -c - | tsp | node ${path.join(__dirname, "/cmds")}/repeat.js "${channel.name}" ${params.ffmpeg} ${tsp_fork_prm.join(" ")}`)
+        tsp_args.push(`tsresync -c - | tsp | node ${path.join(__dirname, "/cmds")}/repeat.js "${channel.name}" ${params.ffmpeg} -progress - -nostats ${tsp_fork_prm.join(" ")}`)
     }
     tsp_args.push("-O")
     tsp_args.push("drop")
