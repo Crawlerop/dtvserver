460c008b9de3427511c833d50a35c23dbfd7f1ea
Your Name <you@example.com>
2022-12-15 17:03:22 +0700

    resmonitoring

--- scripts/dtv.js
+++ scripts/dtv.js
@@ -116,18 +116,14 @@ ExecSignal.once("exec", (args, folders) => {
 RunSignal.once("run", async (params) => {    
    try {
     passed_params = params    
-    var tsp_args = `--buffer-size-mb 64 --max-flushed-packets 25000 --max-output-packets 10000 --max-input-packets 50000 --realtime -I dvb --demux-buffer-size 67108864 --signal-timeout 10 --guard-interval auto --receive-timeout 10000 --adapter ${params.tuner} --delivery-system DVB-T2 --frequency ${params.frequency}000000 --transmission-mode auto --spectral-inversion off`.split(" ")
+    var tsp_args = `${params.dtv_use_fork ? "--monitor " : ""}--buffer-size-mb 64 --max-flushed-packets 25000 --max-output-packets 10000 --max-input-packets 50000 --realtime -I dvb --demux-buffer-size 67108864 --signal-timeout 10 --guard-interval auto --receive-timeout 10000 --adapter ${params.tuner} --delivery-system DVB-T2 --frequency ${params.frequency}000000 --transmission-mode auto --spectral-inversion off`.split(" ")
     //var tsp_args = `--realtime -I dvb --signal-timeout 10 --guard-interval auto --receive-timeout 10000 --adapter ${params.tuner} --delivery-system DVB-T2 --frequency ${params.frequency}000000 --transmission-mode auto --spectral-inversion off`.split(" ")
     var folders = []
     var ad_param;
 
     if (params.additional_params) {
         ad_param = JSON.parse(params.additional_params)
-    }
-
-    if (params.dtv_use_fork) {
-        tsp_args.push("-m")        
-    }
+    }    
 
     for (let i = 0; i<params.channels.length; i++) {
         const channel = params.channels[i]
