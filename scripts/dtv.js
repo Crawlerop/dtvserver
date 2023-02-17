cb541fc2b64b0196850160ed5557f908131f8055
Your Name <you@example.com>
2023-02-17 13:45:22 +0700

    f21512

--- scripts/dtv.js
+++ scripts/dtv.js
@@ -219,8 +219,8 @@ RunSignal.once("run", async (params) => {
                 } else if (params.use_protocol === "rtsp") {
                     tsp_args.push(`tsp -P zap ${channel.id} | ${params.ffmpeg} -copyts -i - -metadata "title=${channel.name}" -vcodec copy -acodec copy -copyinkf -loglevel error -f rtsp -rtsp_transport tcp rtsp://${params.dtv_udp_out[dtv_key]}/`)
                 } else if (params.use_protocol === "udp") {``
-                    //tsp_args.push(`tsp --realtime --buffer-size-mb 8 --max-flushed-packets 7 --max-output-packets 7 --max-input-packets 7 -P zap ${channel.id} -O ip ${params.dtv_udp_out[dtv_key]}`)
-                    tsp_args.push(`tsp -P zap ${channel.id} | ${params.ffmpeg} -copyts -i - -vcodec copy -acodec copy -copyinkf -loglevel error -f mpegts udp://${params.dtv_udp_out[dtv_key]}?pkt_size=1316`)
+                    tsp_args.push(`tsp --realtime --max-flushed-packets 7 --max-output-packets 7 --max-input-packets 7 -P zap ${channel.id} -O ip ${params.dtv_udp_out[dtv_key]}`)
+                    //tsp_args.push(`tsp -P zap ${channel.id} | ${params.ffmpeg} -copyts -i - -vcodec copy -acodec copy -copyinkf -loglevel error -f mpegts udp://${params.dtv_udp_out[dtv_key]}?pkt_size=1316`)
                 } else {
                     throw new Error(`Invalid output protocol: ${params.use_protocol}`)
                 }
