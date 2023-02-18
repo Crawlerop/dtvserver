49531f0e28f0d6fd7574ebcd3c8659c0a3d614dc
Your Name <you@example.com>
2023-02-18 13:45:17 +0700

    nvm

--- scripts/dtv.js
+++ scripts/dtv.js
@@ -192,9 +192,7 @@ RunSignal.once("run", async (params) => {
 
         const do_nvdec_scale = params.do_scale_exclude.indexOf(`${params.frequency}-${channel.id}`) === -1
 
-        const dtv_key = `${params.frequency}-${channel.id}`
-
-        const tsp_fork_prm = ["-y", "-loglevel", "repeat+level+error", "-probesize", Object.keys(params.dtv_udp_out).indexOf(dtv_key) !== -1 ? "512k" : "32", "-analyzeduration", "0"].concat(await ffmp_args.genSingle(params.dtv_use_fork ? "-" : `unix:${LS_SOCKET}`, current_rendition, streams, out_folder, params.hls_settings, -1, channel.audio ? channel.audio.id : -1, audio_filters, passed_params.dtv_use_fork ? true : false, used_watermark, params.do_scale ? do_nvdec_scale : false))
+        const tsp_fork_prm = ["-y", "-loglevel", "repeat+level+error", "-probesize", "32", "-analyzeduration", "0"].concat(await ffmp_args.genSingle(params.dtv_use_fork ? "-" : `unix:${LS_SOCKET}`, current_rendition, streams, out_folder, params.hls_settings, -1, channel.audio ? channel.audio.id : -1, audio_filters, passed_params.dtv_use_fork ? true : false, used_watermark, params.do_scale ? do_nvdec_scale : false))
         
         if (passed_params.dtv_use_fork) {
             tsp_args.push("-P")
@@ -210,7 +208,9 @@ RunSignal.once("run", async (params) => {
             /*
             tsp_args.push("--buffered-packets")
             tsp_args.push("10000")           
-            */                    
+            */
+            
+            const dtv_key = `${params.frequency}-${channel.id}`
 
             if (Object.keys(params.dtv_udp_out).indexOf(dtv_key) !== -1) {
                 if (params.use_protocol === "tcp") {
